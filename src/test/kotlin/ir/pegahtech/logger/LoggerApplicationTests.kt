5e9cd3b39b34177adf9e9f2f33822946c96cab26
Ehsan <ehsansouri23@gmail.com>
2022-11-27 11:57:59 +0330

    Initial commit

---
+++ src/test/kotlin/ir/pegahtech/logger/LoggerApplicationTests.kt
@@ -0,0 +1,12 @@
+package ir.pegahtech.logger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoggerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
