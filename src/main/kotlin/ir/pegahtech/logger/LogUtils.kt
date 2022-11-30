b591e0dd432ec468011bf5951711924622e427ad
Ehsan <ehsansouri23@gmail.com>
2022-11-30 19:27:50 +0330

    Add utils

--- src/main/kotlin/ir/pegahtech/logger/LogUtils.kt
+++ src/main/kotlin/ir/pegahtech/logger/LogUtils.kt
@@ -1,5 +1,7 @@
 package ir.pegahtech.logger
 
+import io.micrometer.core.instrument.MeterRegistry
+import ir.pegahtech.logger.di.meterRegistry
 import java.time.Duration
 
 data class Log(
@@ -13,6 +15,15 @@ data class Log(
 val log: Log
     get() = Log()
 
+val counterLog: Log
+    get() = Log(type = MetricType.Counter)
+
+val timerLog: Log
+    get() = Log(type = MetricType.Timer)
+
+val histogramLog: Log
+    get() = Log(type = MetricType.Histogram)
+
 infix fun Log.withType(type: MetricType): Log =
     copy(type = type)
 
@@ -22,8 +33,18 @@ infix fun Log.withName(name: String): Log =
 infix fun Log.withTags(tags: Map<String, String>): Log =
     copy(tags = tags)
 
+infix fun Log.withTag(tag: () -> Pair<String, String>):Log =
+    copy(tags = tags?.plus(tag()) ?: mapOf(tag()))
+
 infix fun Log.withValue(value: Double): Log =
     copy(value = value)
 
 infix fun Log.withDuration(duration: Duration): Log =
     copy(duration = duration)
+
+infix fun Log.on(meterRegistry: MeterRegistry) =
+    LoggerService.invoke(this, meterRegistry)
+
+fun main() {
+    counterLog withTag {"tag1" to "value1"} withTag {"tag2" to "value2"} withName "counter" withValue 1.0 withDuration Duration.ofSeconds(20) on meterRegistry
+}
